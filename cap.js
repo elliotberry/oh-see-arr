748eebf197493125f3238cbd9e2c38c0c55a7984
elliot b  =^._.^= ∫ <elliot@email.gd>
2024-03-03 23:48:03 -0500

    initialized repo name ocr-capture-to-text-terminal

---
+++ cap.js
@@ -0,0 +1,19 @@
+import screencapture from 'screencapture';
+import fs from 'fs/promises';
+
+const capture = () => {
+  return new Promise((resolve, reject) => {
+    screencapture((err, imagePath) => {
+        fs.readFile(imagePath).then((data) => {
+            resolve([data, imagePath]);
+        });
+    });
+  });
+}
+
+const captureAndDelete = async () => {
+  const [data, imagePath] = await capture();
+  await fs.unlink(imagePath);
+    return data.toString('base64');
+}
+export default captureAndDelete;
---
+++ index.js
@@ -0,0 +1,9 @@
+import { createWorker } from 'tesseract.js';
+const worker = await createWorker('eng');
+import captureAndDelete from './cap.js';
+(async () => {
+  const base64Image = await captureAndDelete();
+    const { data: { text } } = await worker.recognize(`data:image/png;base64,${base64Image}`);
+    console.log(text);
+    await worker.terminate();
+})();
