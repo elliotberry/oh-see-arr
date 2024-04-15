84608c845e58c9e34f61c199b1941358e12b6c52
elliot b  =^._.^= ∫ <elliot@email.gd>
2024-04-15 10:31:50 -0400

    small touches

--- cap.js
+++ cap.js
@@ -1,19 +1,19 @@
-import screencapture from 'screencapture';
-import fs from 'fs/promises';
+import screencapture from "screencapture"
+import fs from "node:fs/promises"
 
 const capture = () => {
   return new Promise((resolve, reject) => {
     screencapture((err, imagePath) => {
-        fs.readFile(imagePath).then((data) => {
-            resolve([data, imagePath]);
-        });
-    });
-  });
+      fs.readFile(imagePath).then((data) => {
+        resolve([data, imagePath])
+      })
+    })
+  })
 }
 
 const captureAndDelete = async () => {
-  const [data, imagePath] = await capture();
-  await fs.unlink(imagePath);
-    return data.toString('base64');
+  const [data, imagePath] = await capture()
+  await fs.unlink(imagePath)
+  return data.toString("base64")
 }
-export default captureAndDelete;
+export default captureAndDelete
