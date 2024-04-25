8ba9920de8a3952c396c47ca6bf80a8b584d14cf
elliot b  =^._.^= ∫ <elliot@email.gd>
2024-04-25 10:59:17 -0400

    ✨ (index.js): add missing import statement for captureAndDelete function to fix the function call in the async IIFE block

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { createWorker } from 'tesseract.js';
 const worker = await createWorker('eng');
 import captureAndDelete from './cap.js';
+
+
 (async () => {
   const base64Image = await captureAndDelete();
     const { data: { text } } = await worker.recognize(`data:image/png;base64,${base64Image}`);
