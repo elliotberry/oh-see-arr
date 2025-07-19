05016bdcf2e3de7252d0316eac7f80aa5f9ad815
ɛʟʟɨօȶ b  =^._.^= ∫ <elliot@email.gd>
2025-07-19 14:11:46 -0400

    unknown progress

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import { createWorker } from "tesseract.js"
 import captureAndDelete from "./capture.js"
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const parent = dirname(__filename);
+const tessdataPath = resolve(`${parent}/tessdata`);
 
 ;(async () => {
-  const worker = await createWorker("eng")
+  const worker = await createWorker("eng", 1, { options: {corePath: tessdataPath, workerPath: tessdataPath, cachePath: tessdataPath, langPath: tessdataPath, dataPath: tessdataPath}})
   const base64Image = await captureAndDelete()
   const {
     data: { text }
